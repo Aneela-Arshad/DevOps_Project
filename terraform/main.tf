65ed23104b386b1d6d0ebc85f4c201546cc2a0b9
Aneela-Arshad <112152429+Aneela-Arshad@users.noreply.github.com>
2023-05-09 01:54:57 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,11 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
 }
+
+data "azurerm_resource_group" "example" {
+  name = azurerm_resource_group.example.name
+}
+
+output "id" {
+  value = data.azurerm_resource_group.example.id
+}
